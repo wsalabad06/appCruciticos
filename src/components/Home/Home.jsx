19c3009172f06b2538a65577d7e21c7117ea3195
warnersalazarbadilla <wasalazarba@est.utn.ac.cr>
2025-03-09 11:16:08 -0600

    test 9-3-25

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+
+import { AspectRatio } from '@mui/joy';
+import Box from '@mui/joy/Box';
+
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL + 'uploads' ;
+
+export default function MinMaxRatio() {
+  return (
+    <Box sx={{ width: 300, resize: 'horizontal', overflow: 'auto', p: 1 }}>
+      <AspectRatio minHeight={120} maxHeight={200}>
+        <img
+          src={`${BASE_URL}/main.jpg`}  // Imagen servida desde el backend
+        />
+      </AspectRatio>
+    </Box>
+  );
+}
 export function Home() {
   return (
     <Container sx={{ p: 2 }} maxWidth="sm">
